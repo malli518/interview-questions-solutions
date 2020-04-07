5e8cfcfa869411f8f87fef1c32b13a075383d260
malli518 <malli.lead518@gmail.com>
2020-04-07 10:39:21 +0530

    added john casino program

---
+++ job-search-pratice/src/main/java/JohnCasino.java
@@ -0,0 +1,31 @@
+
+public class JohnCasino {
+
+	public int solution(int n, int k) {
+		if(k==0) {
+			return n-1;
+		}
+		int count = 0;
+		while (n > 1) {
+			if (k > 0 && n%2 == 0) {
+				n = n / 2;
+				count++;
+				k--;
+			} else {
+				count++;
+				n = n - 1;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		JohnCasino johnCasino=new JohnCasino();
+		System.out.println(johnCasino.solution(18, 2));
+		System.out.println(johnCasino.solution(10, 10));
+		System.out.println(johnCasino.solution(8, 0));
+		
+		System.out.println(johnCasino.solution(8, 2));
+	}
+
+}
